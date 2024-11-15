99db0cff281ffc421b84d369e052ec05ec3e7875
Wanseo <wanseoinbox@gmail.com>
2024-11-15 20:28:48 +0900

    config: configure postgresql database in django settings

--- knyfe/knyfe/settings.py
+++ knyfe/knyfe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "knyfe.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ["DB_NAME"],
+        "USER": os.environ["DB_USER"],
+        "HOST": os.environ["DB_HOST"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "PORT": os.environ["DB_PORT"],
     }
 }
 
