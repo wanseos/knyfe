1a0401edfc42974f1443296c1cd5c49d3c566d17
Wanseo <wanseoinbox@gmail.com>
2024-11-20 13:41:06 +0900

    refactor: remove unused views

--- knyfe/bookings/views/__init__.py
+++ knyfe/bookings/views/__init__.py
@@ -1,9 +1,7 @@
 from . import booking_availability as booking_availability_views
-from .booking_availability_view import BookingAvailabilityListAPIView
 from .booking_view import BookingViewSet
 
 __all__ = [
-    "BookingAvailabilityListAPIView",
     "BookingViewSet",
     "booking_availability_views",
 ]
