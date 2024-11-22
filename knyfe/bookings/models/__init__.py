83776ae8fa934b242ab3bbdd896eb27bff1df0da
Wanseo <wanseoinbox@gmail.com>
2024-11-22 16:43:28 +0900

    refactor: delete deprecated booking model

--- knyfe/bookings/models/__init__.py
+++ knyfe/bookings/models/__init__.py
@@ -1,11 +1,9 @@
-from .booking import Booking
 from .booking_event import BookingEvent
 from .booking_projection import BookingProjection
 from .user import User
 
 __all__ = [
     "User",
-    "Booking",
     "BookingEvent",
     "BookingProjection",
 ]
